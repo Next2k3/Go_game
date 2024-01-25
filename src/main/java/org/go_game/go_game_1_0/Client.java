71a37f93b3b8f702f568c152ba5deecd5b6958be
BartoszJO <bartoszjolszewski@hotmail.com>
2024-01-25 12:08:04 +0100

    Częsciowa implementacja sendMove, receiveInfoFromServer oraz receiveMove

--- src/main/java/org/go_game/go_game_1_0/Client.java
+++ src/main/java/org/go_game/go_game_1_0/Client.java
@@ -2,9 +2,11 @@ package org.go_game.go_game_1_0;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.go_game.go_game_1_0.Board.Stone;
+import org.go_game.go_game_1_0.Board.StoneColor;
+
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class Client extends Application implements Runnable {
     private static final String SERVER_ADDRESS = "localhost";
@@ -19,10 +21,15 @@ public class Client extends Application implements Runnable {
     private boolean myTurn = false;
     private boolean waiting = true;
     private boolean continueToPlay = true;
+    private Stone [][] stone = new Stone[9][9];
+    private int rowSelected;
+    private int columnSelected;
 
     private DataInputStream fromServer;
     private DataOutputStream toServer;
     Socket socket;
+
+    StoneColor stoneColor;
 
 
 
@@ -32,12 +39,14 @@ public class Client extends Application implements Runnable {
         display.init();
 
         // Uruchamianie JavaFX w głównym wątku
-        // launch(args);
+         launch(args);
     }
 
     public void init() {
         // inicjalizacja planszy do gry
         // i całego okienka (tytul, przzyciski itd.)
+
+
 
         connectToServer();
     }
@@ -95,14 +104,45 @@ public class Client extends Application implements Runnable {
     }
 
     private void sendMove() throws IOException {
-
+        toServer.writeInt(rowSelected);
+        toServer.writeInt(columnSelected);
     }
 
     private void receiveInfoFromServer() throws IOException {
+        int status = fromServer.readInt();
+        if (status == PLAYER1_WON) {
+            continueToPlay = false;
+            if (stoneColor == StoneColor.WHITE) {
+                // wyswietlenie na planszy wygranego i jego koloru
+            } else if (stoneColor == StoneColor.BLACK) {
+                // wyswietlenie ze przeciwnik wygral
+                receiveMove();
+            }
+        } else if (status == PLAYER2_WON) {
+            continueToPlay = false;
+            if (stoneColor == StoneColor.WHITE) {
+                // wyswietlenie wygranej i koloru gracza
+            } else if (stoneColor == StoneColor.BLACK) {
+                // wyswietlenie wygranej przeciwnika i jego koloru
+                receiveMove();
+            }
+        } else if (status == DRAW ) {
+            continueToPlay = false;
+            // wyswietlenie informacji o remisie
 
+            if (stoneColor == StoneColor.WHITE) {
+                receiveMove();
+            } else {
+                receiveMove();
+                // wyswietlenie: Moja tura
+                myTurn = true;
+            }
+        }
     }
 
     private void receiveMove() throws IOException {
+        int row = fromServer.readInt();
+        int column = fromServer.readInt();
 
     }
 
