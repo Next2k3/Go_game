c2ad3d328cf5b6c0bf032a0816077eebfa31091a
BartoszJO <bartoszjolszewski@hotmail.com>
2024-01-25 12:09:34 +0100

    Dodanie zmiennych i edytowany konstruktor Stone (dodanie czesciowej zaleznosci z Okręgami)

--- src/main/java/org/go_game/go_game_1_0/Board/Stone.java
+++ src/main/java/org/go_game/go_game_1_0/Board/Stone.java
@@ -1,16 +1,34 @@
 package org.go_game.go_game_1_0.Board;
 
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class Stone {
     private StoneColor stoneColor;
     private StoneGroup stoneGroup;
     private int row;
     private int col;
+    private Circle circle;
 
-    public Stone(StoneColor stoneColor,int row,int col){
+    public Stone(StoneColor stoneColor,int row,int col, double radius){
         this.stoneColor = stoneColor;
         this.row = row;
         this.col = col;
+        this.circle = new Circle(radius);
+        if (stoneColor == StoneColor.BLACK) {
+            this.circle.setFill(Color.BLACK);
+        } else {
+            this.circle.setFill(Color.WHITE);
+        }
+    }
+
+    public Circle getCircle() {
+        return circle;
+    }
+
+    public void setCircle(Circle circle) {
+        this.circle = circle;
     }
     public void setStoneGroup(StoneGroup stoneGroup){
         this.stoneGroup = stoneGroup;
--- src/main/java/org/go_game/go_game_1_0/Board_2/StoneColor.java
+++
@@ -1,6 +0,0 @@
-package org.go_game.go_game_1_0.Board_2;
-
-public enum StoneColor {
-    WHITE,
-    BLACK
-}
