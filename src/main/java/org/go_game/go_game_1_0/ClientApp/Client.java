37a351cbe7faa86f89f501d8a9c9f81831de3584
BartoszJO <bartoszjolszewski@hotmail.com>
2024-01-29 22:36:56 +0100

    Working client-server communication - working game (just turns)

--- src/main/java/org/go_game/go_game_1_0/ClientApp/Client.java
+++ src/main/java/org/go_game/go_game_1_0/ClientApp/Client.java
@@ -54,21 +54,127 @@ public class Client extends Application {
 
             myColor = in.readLine();
             myTurn = myColor.equals("CZ");
-            GridPane grid = new GridPane();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    circles[i][j] = new Circle(25, Color.BROWN);
-                    circles[i][j].setStroke(Color.BLACK);
-                    int finalI = i;
-                    int finalJ = j;
-                    circles[i][j].setOnMouseClicked(e -> makeMove(finalI, finalJ));
-                    grid.add(circles[i][j], j, i);
+//            GridPane grid = new GridPane();
+//            for (int i = 0; i < 9; i++) {
+//                for (int j = 0; j < 9; j++) {
+//                    circles[i][j] = new Circle(25, Color.BROWN);
+//                    circles[i][j].setStroke(Color.BLACK);
+//                    int finalI = i;
+//                    int finalJ = j;
+//                    circles[i][j].setOnMouseClicked(e -> makeMove(finalI, finalJ));
+//                    grid.add(circles[i][j], j, i);
+//                }
+//            }
+//            Scene scene = new Scene(grid, 600, 600);
+//            primaryStage.setTitle("Gra GO - " + myColor);
+//            primaryStage.setScene(scene);
+//            primaryStage.show();
+
+        int size=9;
+        StoneColor[][] stoneColors = new StoneColor[size][size];
+
+        ObservableList<String> moveHistory = FXCollections.observableArrayList();
+        ListView<String> moveHistoryListView = new ListView<>(moveHistory);
+
+        HBox hBox = new HBox();
+        hBox.setBackground(Background.fill(Color.rgb(26,26,26)));
+
+        GridPane gridPane = new GridPane();
+        gridPane.setStyle("-fx-background-color: #DEB887");
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++) {
+                circles[i][j] = new Circle(17.5, Color.BURLYWOOD);
+
+                circles[i][j].setStroke(Color.rgb(26,26,26));
+                circles[i][j].setStrokeWidth(3);
+                if(stoneColors[i][j]==null) {
+                    circles[i][j].setFill(Color.BURLYWOOD);
+                }else if(stoneColors[i][j]== StoneColor.WHITE){
+                    circles[i][j].setFill(Color.WHITE);
+                }else{
+                    circles[i][j].setFill(Color.BLACK);
                 }
+
+//                circle.setOnMouseClicked(e->{
+//                            int x= (int) Math.round(e.getSceneX()/55.0)-1;
+//                            int y= (int) Math.round(e.getSceneY()/55.0)-1;
+//                            x = Math.max(0, x);
+//                            y = Math.max(0, y);
+//                            handleButtonClick(x,y);
+//                            updateCircle(x, y);
+//                            String moveInfo = "(" + x + ", " + y + ")";
+//                            moveHistory.add(moveInfo);
+//                 });
+
+                Line lineV = new Line();
+                lineV.setStartX(25);
+                lineV.setStartY(0);
+                lineV.setEndX(25);
+                lineV.setEndY(50);
+
+                Line lineH = new Line();
+                lineH.setStartX(0);
+                lineH.setStartY(25);
+                lineH.setEndX(50);
+                lineH.setEndY(25);
+
+                lineV.setStrokeWidth(5);
+                lineH.setStrokeWidth(5);
+
+                int finalI = i;
+                int finalJ = j;
+                circles[i][j].setOnMouseClicked(e -> makeMove(finalI, finalJ));
+                StackPane stackPane = new StackPane(lineH,lineV,circles[i][j]);
+                gridPane.add(stackPane, j, i);
+
+                GridPane.setHalignment(stackPane, HPos.CENTER);
+                GridPane.setValignment(stackPane, VPos.CENTER);
+
             }
-            Scene scene = new Scene(grid, 600, 600);
-            primaryStage.setTitle("Gra GO - " + myColor);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        }
+        gridPane.setMaxWidth(450);
+        gridPane.setMaxHeight(450);
+
+        VBox vBox = new VBox(15);
+
+        moveHistoryListView.setStyle("-fx-background-color: #262626");
+        moveHistoryListView.setMinHeight(450);
+
+        vBox.getChildren().add(moveHistoryListView);
+
+        HBox hBox1 = new HBox(15);
+
+        Button giveUpButton = new Button("Przerwij");
+        giveUpButton.setFont(new Font(15));
+        giveUpButton.setMinWidth(150);
+        giveUpButton.setStyle("-fx-background-color: #595959");
+        giveUpButton.setTextFill(Color.WHITE);
+
+        giveUpButton.setOnMouseEntered(e -> giveUpButton.setStyle("-fx-background-color: lightgray;"));
+        giveUpButton.setOnMouseExited(e -> giveUpButton.setStyle("-fx-background-color: #595959;"));
+
+        Button rematchButton = new Button("Rewanż");
+        rematchButton.setFont(new Font(15));
+        rematchButton.setMinWidth(150);
+        rematchButton.setStyle("-fx-background-color: #595959");
+        rematchButton.setTextFill(Color.WHITE);
+
+        rematchButton.setOnMouseEntered(e -> rematchButton.setStyle("-fx-background-color: lightgray;"));
+        rematchButton.setOnMouseExited(e -> rematchButton.setStyle("-fx-background-color: #595959;"));
+
+        HBox hBox2 = new HBox(gridPane);
+        hBox2.setPadding(new Insets(15));
+        hBox.getChildren().add(hBox2);
+        hBox1.getChildren().add(giveUpButton);
+        hBox1.getChildren().add(rematchButton);
+        vBox.getChildren().add(hBox1);
+        vBox.setPadding(new Insets(15));
+        hBox.getChildren().add(vBox);
+        Scene gameScene = new Scene(hBox);
+
+        primaryStage.setScene(gameScene);
+        primaryStage.show();
 
             new Thread(this::run).start();
 
@@ -78,7 +184,7 @@ public class Client extends Application {
     }
 
     private void makeMove(int i, int j) {
-        if (myTurn && circles[i][j].getFill() == Color.BROWN) {
+        if (myTurn && circles[i][j].getFill() == Color.BURLYWOOD) {
             Color color = myColor.equals("CZ") ? Color.BLACK : Color.WHITE;
             circles[i][j].setFill(color);
             out.println(i + "," + j);
